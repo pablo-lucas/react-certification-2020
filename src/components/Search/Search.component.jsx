8a1be0d64a17c74766f14b86e3fb6049ab09d671
Pablo Lucas <pablo.lucas@wizeline.com>
2020-09-21 13:11:58 -0500

    feat: added youtube api client

--- src/components/Search/Search.component.jsx
+++ src/components/Search/Search.component.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import { Input } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Form, Input } from 'semantic-ui-react';
 
 const Search = () => {
-  return <Input icon="search" placeholder="Search..." />;
+  const [term, setTerm] = useState('');
+
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <Form.Field>
+      <Input
+        type="text"
+        value={term}
+        onChange={onInputChange}
+        icon="search"
+        placeholder="Search..."
+      />
+    </Form.Field>
+  );
 };
 
 export default Search;
